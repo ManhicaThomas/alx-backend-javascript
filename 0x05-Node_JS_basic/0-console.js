24e51e7d32e5927443733d48dd04c09d37160557
ManhicaThomas <thomasmanchica@gmail.com>
2024-09-16 13:22:09 -0700

    update

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Thomas Manhica <https://github.com/ManhicaThomas>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
